abb11ead5eb655cc0d8943aaa2fbb98e824c1bb3
Bryushkov Ilya <bryushkov.i@yandex.ru>
2024-02-24 22:27:09 +0300

    Свойства Клиента и Отдела

---
+++ HomeWork14/HomeWork14/Models/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14.Models
+{   
+    /// <summary>
+    /// Клинет
+    /// </summary>
+    abstract class Client
+    {
+        private DateTime _CreateDate;
+        /// <summary>
+        /// Дата создания УЗ клиента
+        /// </summary>
+        protected DateTime CreateDate { get => _CreateDate; set => _CreateDate = value; }
+        
+        private string _Name;
+        /// <summary>
+        /// Имя
+        /// </summary>
+        protected string Name { get => _Name; set => _Name = value; }
+
+
+        private double _Deposit;
+        /// <summary>
+        /// Вклад
+        /// </summary>
+        protected double Deposit { get => _Deposit; set => _Deposit = value; }
+
+
+        private double _Credit;
+        /// <summary>
+        /// Кредит
+        /// </summary>
+        protected double Credit { get => _Credit; set => _Credit = value; }
+
+
+        private double _Debt;
+        /// <summary>
+        /// Дебит (баланс)
+        /// </summary>
+        protected double Debt { get => _Debt; set => _Debt = value; }
+
+        private double _CreditPercent;
+        /// <summary>
+        /// Процент по кредиту
+        /// </summary>
+        protected virtual double CreditPercent { get => _CreditPercent; set => _CreditPercent = value; }
+
+
+        private double _DepositPercent;
+        protected virtual double DepositePercent { get => _DepositPercent; set => _DepositPercent = value; }
+
+    }
+}
---
+++ HomeWork14/HomeWork14/Models/Department.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14.Models
+{
+    class Department
+    {
+        private double _KeyRate;
+        
+        
+
+        private int _DepartmentType;
+        /// <summary>
+        /// Тип отдела
+        /// </summary>
+        public int DepartmentType { get => _DepartmentType; set => _DepartmentType = value; }
+
+        private double _BaseDepartmentCreditPercent;
+        /// <summary>
+        /// Базовая ставка по кредиту в отделе
+        /// </summary>
+        public double BaseDepartmentCreditPercent
+        {
+            get => _BaseDepartmentCreditPercent;
+            set
+            {
+                switch (_DepartmentType)
+                {
+                    case 1:
+                        _BaseDepartmentCreditPercent = _KeyRate + 0.5;
+                        break;
+                    case 2:
+                        _BaseDepartmentCreditPercent = _KeyRate + 1.5;
+                        break;
+                    case 3:
+                        _BaseDepartmentCreditPercent = _KeyRate + 1;
+                        break;
+                    default:
+                        _BaseDepartmentCreditPercent = _KeyRate;
+                        break;
+                }
+            }
+        }
+
+        private double _BaseDepartmentDepositPercent;
+        public double BaseDepartmentDepositPercent
+        {
+            get => _BaseDepartmentDepositPercent;
+            set
+            {
+                switch (_DepartmentType)
+                {
+                    case 1:
+                        _BaseDepartmentDepositPercent = _KeyRate - 0.5;
+                        break;
+                    case 2:
+                        _BaseDepartmentDepositPercent = _KeyRate - 1.5;
+                        break;
+                    case 3:
+                        _BaseDepartmentDepositPercent = _KeyRate - 1;
+                        break;
+                    default:
+                        _BaseDepartmentDepositPercent = _KeyRate;
+                        break;
+                }
+            }
+        }
+    }
+}
